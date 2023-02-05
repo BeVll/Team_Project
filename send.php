19836ccc4dd3a5beeb9acb91385563c39b1f9f24
Bevll <you@example.com>
2023-02-06 00:51:35 +0200

    fix

--- send.php
+++ send.php
@@ -7,7 +7,9 @@ if (isset($_POST["username"]) && isset($_POST["number"])) {
     }
     $name = $conn->real_escape_string($_POST["username"]);
     $number = $conn->real_escape_string($_POST["number"]);
-    $dt = date('d-m-y h:i:s');
+    
+    $dt = date('y-m-d h:i:s');
+  
     $sql = "INSERT INTO requests (name, phone, time) VALUES ('$name', '$number', '$dt')";
     if($conn->query($sql)){
         readfile('newpage.html');
